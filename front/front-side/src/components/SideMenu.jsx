f4ab6c182c70cf1e0fcf545edaf6e5c812ae62c4
Salman-Ali05 <sallukhan0805@gmail.com>
2024-04-17 16:04:08 +0200

    FRONT : Base settled

---
+++ front/front-side/src/components/SideMenu.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function SideMenu() {
+  return (
+    <div className="side-menu">
+
+        <div className="menu-items">Home</div>
+        <div className="menu-items">Login</div>
+        <div className="menu-items">User List</div>
+
+      </div>
+  )
+}
+
+export default SideMenu
