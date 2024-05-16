8ba12821970e0c9ef63599376b4cbf6457cefab3
masterivanic <masterivanic@gmail.com>
2024-05-16 14:24:48 +0200

    dockeriez application front done

--- front/front-side/src/pages/UserList.jsx
+++ front/front-side/src/pages/UserList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useAuth } from '../AuthContext'; 
 
 function UserList() {
     const [users, setUsers] = useState([]);
@@ -8,69 +9,43 @@ function UserList() {
     useEffect(() => {
         const fetchUsers = async () => {
             try {
-                const response = await axios.get('http://localhost:8000/api/users/',
+                const response = await axios.get('http://localhost:8000/api/users/', {
                     headers: {
-                    'Authorization': `Estiam ${user.access}`
+                        'Authorization': `Estiam ${user.access}`
+                    }
                 });
-                setUsers(response.data);
+
+                if (response.status === 200) {
+                    setUsers(response.data);
+                } else {
+                    console.error('Erreur lors de la récupération de la liste des utilisateurs. Statut:', response.status);
+                }
             } catch (error) {
                 console.error('Erreur lors de la récupération de la liste des utilisateurs :', error);
             }
         };
 
         fetchUsers();
-    }, []);
+    }, [user.access]); // Ajoutez user.access comme dépendance pour recharger la liste des utilisateurs lorsque l'utilisateur change
 
     return (
         <div className='user-list'>
-
             <div className='user-list-main-title'>
                 <h1>USERS LIST</h1>
             </div>
 
             <div className='user-list-table'>
-
                 {users.map(user => (
                     <div key={user.id} className='u-items'>
                         <p>{user.email}</p>
                         <p>{user.username}</p>
                         <p className='filler'></p>
                     </div>
-                    <hr />
                 ))}
-                
+                <hr />
             </div>
-
         </div>
     );
 }
 
 export default UserList;
-
-// import React from 'react'
-
-// function UserList() {
-//     return (
-//         <div className='user-list'>
-
-//             <div className='user-list-main-title'>
-//                 <h1>USERS LIST</h1>
-//             </div>
-
-//             <div className='user-list-table'>
-
-//                 <div className='u-items'>
-//                     <p>Email</p>
-//                     <p>Username</p>
-//                     <p className='filler'></p>
-//                 </div>
-
-//                 <hr />
-
-//             </div>
-
-//         </div>
-//     )
-// }
-
-// export default UserList
